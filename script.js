6906f442d976992c206e84ed753897b0229acbaa
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-06-09 20:49:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,30 @@
-let passwordInput = document.querySelector(
-  '#passwordInput input[type="password"]'
-);
-let passwordStrength = document.getElementById("passwordStrength");
-let poor = document.querySelector("#passwordStrength #poor");
-let weak = document.querySelector("#passwordStrength #weak");
-let strong = document.querySelector("#passwordStrength #strong");
-let passwordInfo = document.getElementById("passwordInfo");
+const pwd = document.getElementById("pwd_txt");
+const generate = document.getElementById("generate");
+const clipboard = document.getElementById("clipboard");
+let pwd_length = document.getElementById("slider");
+let copy_text = document.getElementById("copy_text");
+let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
 
-let poorRegExp = /[a-z]/;
-let weakRegExp = /(?=.*?[0-9])/;
-let strongRegExp = /(?=.*?[#?!@$%^&*-])/;
-let whitespaceRegExp = /^$|\s+/;
-
-passwordInput.oninput = function () {
-  let passwordValue = passwordInput.value;
-  let passwordLength = passwordValue.length;
-
-  let poorPassword = passwordValue.match(poorRegExp);
-  let weakPassword = passwordValue.match(weakRegExp);
-  let strongPassword = passwordValue.match(strongRegExp);
-  let whitespace = passwordValue.match(whitespaceRegExp);
-
-  if (passwordValue != "") {
-    passwordStrength.style.display = "block";
-    passwordStrength.style.display = "flex";
-    passwordInfo.style.display = "block";
-    passwordInfo.style.color = "black";
-
-    if (whitespace) {
-      passwordInfo.textContent = "Whitespaces are not allowed";
-    } else {
-      poorPasswordStrength(
-        passwordLength,
-        poorPassword,
-        weakPassword,
-        strongPassword
-      );
-      weakPasswordStrength(
-        passwordLength,
-        poorPassword,
-        weakPassword,
-        strongPassword
-      );
-      strongPasswordStrength(
-        passwordLength,
-        poorPassword,
-        weakPassword,
-        strongPassword
-      );
-    }
-  } else {
-    passwordStrength.style.display = "none";
-    passwordInfo.style.display = "none";
+generate.addEventListener("click", () => {
+  let password = "";
+  let checked = document.getElementById("checkbox").checked;
+  let final_string = string;
+  if (checked) {
+    final_string += "@#$%^&*/=+?";
   }
-};
-
-function poorPasswordStrength(
-  passwordLength,
-  poorPassword,
-  weakPassword,
-  strongPassword
-) {
-  if (passwordLength <= 3 && (poorPassword || weakPassword || strongPassword)) {
-    poor.classList.add("active");
-    passwordInfo.style.display = "block";
-    passwordInfo.style.color = "red";
-    passwordInfo.textContent = "Your password is poor";
+  for (let i = 0; i < pwd_length.value; i++) {
+    let pwd = final_string[Math.floor(Math.random() * final_string.length)];
+    password += pwd;
   }
-}
+  pwd.innerText = password;
+  final_string = string;
+});
 
-function weakPasswordStrength(
-  passwordLength,
-  poorPassword,
-  weakPassword,
-  strongPassword
-) {
-  if (passwordLength >= 4 && poorPassword && (weakPassword || strongPassword)) {
-    weak.classList.add("active");
-    passwordInfo.textContent = "Your password is Weak";
-    passwordInfo.style.color = "orange";
-  } else {
-    weak.classList.remove("active");
-  }
-}
-
-function strongPasswordStrength(
-  passwordLength,
-  poorPassword,
-  weakPassword,
-  strongPassword
-) {
-  if (passwordLength >= 6 && poorPassword && weakPassword && strongPassword) {
-    poor.classList.add("active");
-    weak.classList.add("active");
-    strong.classList.add("active");
-    passwordInfo.textContent = "Your password is strong";
-    passwordInfo.style.color = "green";
-  } else {
-    strong.classList.remove("active");
-  }
-}
-
-let showHide = document.querySelector("#passwordInput #showHide");
-
-showHide.onclick = function () {
-  showHidePassword();
-};
-
-function showHidePassword() {
-  if (passwordInput.type == "password") {
-    passwordInput.type = "text";
-    showHide.textContent = "HIDE";
-    showHide.style.color = "green";
-  } else {
-    passwordInput.type = "password";
-    showHide.textContent = "SHOW";
-    showHide.style.color = "red";
-  }
-}
+clipboard.addEventListener("click", () => {
+  navigator.clipboard.writeText(pwd.innerText);
+  copy_text.textContent = "Password Copied!";
+  copy_text.style.display = "block";
+  setTimeout(() => {
+    copy_text.style.display = "none";
+  }, 2000);
+});
